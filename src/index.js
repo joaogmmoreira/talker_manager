efd490fa1815a08df824167d2fe6334f0d64a8a7
Joao Moreira <jgmoreira1809@gmail.com>
2022-10-04 15:56:28 -0300

    Requisito 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,7 +8,15 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-//  não remova esse endpoint, e para o avaliador funcionar
+app.get('/talker', async (_req, res) => {
+  const data = await fs.readFile('/app/src/talker.json');
+  if (!data) {
+    return [];
+  }
+  return res.status(HTTP_OK_STATUS).json(JSON.parse(data));
+});
+
+// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
